626e6be8ba104179abda6ba6ddb552137b41eaf1
James Sharpe <jameswrightsharpe@gmail.com>
2020-01-09 18:19:27 -0500

    adding animations to main dic

--- src/js/index.js
+++ src/js/index.js
@@ -5,17 +5,41 @@ window.onload = function() {
     onprogress: function(state, progress) {
       console.log(state, progress);
     },
-    onsuccess: function() {
-      anime({
-        targets: "#keys",
-        height: "160px",
-        duration: 1200,
-        easing: "easeInOutQuad",
-        delay: "100",
-        complete: function(anim) {
-          console.log("done");
-        }
-      });
-    }
+    onsuccess: animatePiano
+  });
+};
+
+const animatePiano = () => {
+  anime({
+    targets: "#keys",
+    height: "160px",
+    duration: 800,
+    easing: "easeInOutQuad",
+    delay: "150",
+    complete: animateMain
+  });
+};
+const animateMain = () => {
+  document.getElementById("main").style = "";
+  anime({
+    targets: "#main",
+    translateX: "120px",
+    opacity: 0,
+    duration: 2000,
+    easing: "easeInOutQuad",
+    delay: "150",
+    direction: "reverse",
+    complete: animateActions
+  });
+};
+const animateActions = () => {
+  anime({
+    targets: "li",
+    translateX: "120px",
+    opacity: 0,
+    duration: 2000,
+    easing: "easeInOutQuad",
+    direction: "reverse",
+    delay: "150"
   });
 };
--- src/js/piano.js
+++ src/js/piano.js
@@ -1,14 +1,28 @@
-const playNoteByKey = key => {
-  const delay = 0;
-  const velocity = 63;
-  // play the note
-  MIDI.setVolume(0, 127);
-  MIDI.noteOn(0, key, velocity, delay);
-  MIDI.noteOff(0, key, delay + 0.75);
-};
-
-const keys = document.getElementsByClassName("key");
-
-for (let i = 0; keys.length; i++) {
-  keys[i].addEventListener("click", () => playNoteByKey(i + 50));
+function inputNoteOn(key) {
+  APP.keyboardFlags[key] = {
+    time: Date.now()
+  };
+  MIDI.setVolume(APP.cid, APP.volume);
+  MIDI.noteOn(APP.cid, key, APP.velocity, 0);
 }
+
+function inputNoteOff(key) {
+  if (APP.keyboardFlags[key]) {
+    MIDI.noteOff(APP.cid, key, 0);
+    delay = (Date.now() - APP.keyboardFlags[key].time) / 1000.0;
+    APP.keyboardFlags[key] = null;
+  }
+}
+
+document.body.addEventListener("keydown", e => {
+  const key = APP.notesByKey[e.key];
+  if (!APP.keyboardFlags[key]) {
+    inputNoteOn(key);
+  }
+});
+document.body.addEventListener("keyup", e => {
+  const key = APP.notesByKey[e.key];
+  if (APP.keyboardFlags[key]) {
+    inputNoteOff(key);
+  }
+});
