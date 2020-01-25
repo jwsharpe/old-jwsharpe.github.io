c6b229125847b2cfdb901fb11cb4ec6056dff493
James Sharpe <jameswrightsharpe@gmail.com>
2020-01-25 14:38:13 -0500

    added a build of YNPT

---
+++ your-next-page-turner/precache-manifest.7289192aa62c2ca355b00bee9c25a908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80722171e020ee55c5d45787f10c954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d30a9b1f0c868e09ee76",
+    "url": "/static/css/main.73c5b3e4.chunk.css"
+  },
+  {
+    "revision": "dee685363418e997b88b",
+    "url": "/static/js/2.eea42066.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.eea42066.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d30a9b1f0c868e09ee76",
+    "url": "/static/js/main.4521d076.chunk.js"
+  },
+  {
+    "revision": "05a66df2b6ae37e5d0fc",
+    "url": "/static/js/runtime-main.2c8291dd.js"
+  }
+]);
